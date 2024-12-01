a254f77060d19b62a8518f48c482cc63253eb3e7
DEEPAK-223-BIT <deepakchennaboina@gmail.com>
2024-12-01 21:59:06 +0530

    defaultvalob

---
+++ alerts/alerts.js
@@ -0,0 +1,4 @@
+alert("you are in wrong website");
+prompt("what is your age",[18,"you notey"]);
+let a =confirm("are you 18+");
+console.log(a);
---
+++ objects/objectcreation.js
@@ -0,0 +1,9 @@
+let car ={
+    brand :"rr",
+    model :"r10",
+    year :2024
+}
+car.model="rr310"
+console.log(car.brand);
+console.log(car.model);
+console.log(car.year);
